50b0bab2d4fee4709e63ecd3ea2b652146676f82
JoshuaGWalker <135189481+JoshuaGWalker@users.noreply.github.com>
2023-06-02 00:08:45 -0400

    Update app.js
    Working to create a randomization of a stack of cards.

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ element.addEventListener("click", function() {
   element.setAttribute("rank", "queen");
 });
 
+var stack_of_cards =  [
+{suite : "hearts", rank : "2"},
+{suite : "hearts", rank : "3"}
+];
+var card_drawn = document.getElementById("card_from_stack");
+card_drawn.addEventListener("click", function() {
+  card_drawn.setAttribute("suite", stack_of_cards[1].suite);
+  card_drawn.setAttribute("rank", stack_of_cards[1].rank);
+});
